c03183afe4bb5b3618f6ee1e01d7da922ef876ac
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-06-14 18:48:37 +0500

    fix: remove logo from subscribe success page since layout is wrapped

--- src/resources/views/frontend/themes/default/sections/subscribe-success.blade.php
+++ src/resources/views/frontend/themes/default/sections/subscribe-success.blade.php
@@ -6,9 +6,7 @@
         <div class="col-md-6">
             <div class="card text-center">
                 <div class="card-body p-5">
-                    @if(isset($logo))
-                        <img src="{{ $logo }}" alt="Logo" class="mb-3" style="max-width: 150px;">
-                    @endif
+
                     <h5 class="card-title">{{ $title ?? translate('Success') }}</h5>
                     <p class="card-text my-4">{{ $message ?? translate('Operation completed successfully.') }}</p>
                     <a href="{{ url('/') }}" class="i-btn btn--primary bg--gradient btn--xl pill w-max-content">{{ translate('Go to Homepage') }}</a>
